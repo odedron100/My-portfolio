1b191d576f648f68dc9439a00ffa871a101bd2a6
odedron100 <odedron84@gmail.com>
2021-02-01 16:37:42 +0200

    Render projects in page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-console.log('Starting up');
+
+'use strict';
+
+$(document).ready(init);
+
+function init() {
+  renderProjects();
+}
+
+function renderProjects() {
+  var projects = getProjects();
+  var strHTML = projects.map(function (project) {
+    return `
+      <div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
+          </div>
+          <img class="img-fluid imgs" src=${project.imgUrl} alt="">
+            </a>
+          <div class="portfolio-caption">
+            <h4>${project.name}</h4>
+            <p class="text-muted">${project.title}</p>
+          </div>
+    `
+  })
+  var $elRow = $('#portfolio .rowProjects');
+  $elRow.html(strHTML);
+}
