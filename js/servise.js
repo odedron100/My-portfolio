7cab784d40da1ce421d07cb7d98bfc70bd267c55
odedron100 <odedron84@gmail.com>
2021-02-01 16:38:23 +0200

    img

---
+++ js/servise.js
@@ -0,0 +1,59 @@
+
+
+var gProjects = [
+  {
+    id: 'book show',
+    name: 'Book Show',
+    title: 'Management and organization of a bookstore',
+    desc: 'If you need help ordering and organizing in your bookstore, place where the user can see the books, prices, and description on the books.This is the project for you!',
+    imgUrl: 'img/portfolio/books-logo.png',
+    url: 'https://odedron100.github.io/Book-shop/',
+    publishedAt: 1448693940000,
+    // labels: ['Matrixes', 'keyboard events'],
+  },
+  {
+    id: 'chat',
+    name: 'Chat',
+    title: 'Chat with a service representative',
+    desc: 'If you need help, you can go to the website and talk to a live service representative who will help you solve the problems!The service representative can route the calls and the calls are saved so that they can go over the calls',
+    imgUrl: 'img/portfolio/messanger.jpg',
+    url: 'https://odedron100.github.io/Book-shop/',
+    publishedAt: 1448693940000,
+    // labels: ['Matrixes', 'keyboard events'],
+  },
+  {
+    id: 'mine sewwped',
+    name: 'Mine sewwped',
+    title: 'Thinking game',
+    desc: 'If you like games that combine thinking, animation and competition.This is the game for you!',
+    imgUrl: 'img/portfolio/mines-sweeper-logo.png',
+    url: 'https://odedron100.github.io/mine-swepped---oded-ron/',
+    publishedAt: 1448693940000,
+    // labels: ['Matrixes', 'keyboard events'],
+  },
+  {
+    id: 'touch nums',
+    name: 'Touch nums',
+    title: 'Thinking game for kids',
+    desc: 'A thinking game for children that combines thinking and knowing numbers according to degrees of difficulty',
+    imgUrl: 'img/portfolio/numbers-logo.jpeg',
+    url: 'https://odedron100.github.io/touch-nums/',
+    publishedAt: 1448693940000,
+    // labels: ['Matrixes', 'keyboard events'],
+  },
+  {
+    id: 'pacman',
+    name: 'Pacman',
+    title: 'Game',
+    desc: 'Familiar Pacman game with animations and upgrades',
+    imgUrl: 'img/portfolio/pacman2.jpeg',
+    url: 'https://odedron100.github.io/Pacman/',
+    publishedAt: 1448693940000,
+    // labels: ['Matrixes', 'keyboard events'],
+  },
+]
+
+
+function getProjects() {
+  return gProjects;
+}
